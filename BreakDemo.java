e66a1fdb3c374d7a640ebe4e33b5aaa4138a94d9
Sagar Chakilala - Automation <sagarch.aaro@gmail.com>
2021-01-20 21:41:12 +0530

    Update BreakDemo.java

--- BreakDemo.java
+++ BreakDemo.java
@@ -6,31 +6,29 @@ public class BreakDemo {
 		
 		System.out.println("Before Break Execution");
 		for(int i=1;i<=10;i++){
-			System.out.println("Hello World....!!!");//1,2,3,4,5
 			
 			if(i==5){
 				break;
 			}
 			
-			System.out.println("i Value is :"+i);//1,2,3,4
+			System.out.println("i Value is :"+i);
 		}
 		
-		System.out.println("After Break Execution");
+		System.out.println("Break Execution");
 	}
 }
 
 class BreakDemo1 {
 	public static void main(String[] args) {
-		for(int i=1;i<=3;i++){//Outer Loop
+		for(int i=1;i<=3;i++){
 			
-			for(int j=1;j<=10;j++){//Inner Loop
+			for(int j=1;j<=10;j++){
 				
 				if(j==4){
 					break;
 				}
-				System.out.println(j);	//1,2,3,
-										//1,2,3
-										//1,2,3
+				System.out.println(j);											
+										
 			}			
 		}
 	}
